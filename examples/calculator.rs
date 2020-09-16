0de4bd0a2dff2f470dd5029f6afa137102e37691
0yoyoyo <60439919+0yoyoyo@users.noreply.github.com>
2020-09-16 20:09:20 +0900

    Add input/output loop

--- examples/calculator.rs
+++ examples/calculator.rs
@@ -1,6 +1,22 @@
+use std::io::{self, Write};
 use calculator::interpret;
 
 fn main() {
-    let result = interpret("1 + 2 * 3");
-    println!("{}", result);
+    loop {
+        let mut output = io::stdout();
+        output.write(b"> ").unwrap();
+        output.flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        match input {
+            "quit" => break,
+            _ => {
+                let result = interpret(&input);
+                println!("{}", result);
+            }
+        }
+    }
 }
